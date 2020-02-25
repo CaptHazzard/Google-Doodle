d217c5dd49e1afd688156b0a5fae132459cfec69
CaptHazzard <59583076+CaptHazzard@users.noreply.github.com>
2020-02-24 19:20:31 -0800

    Added java animations and positioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,44 @@
 
+gsap.fromTo(".block", {y:-1000}, {y: 0});
+gsap.fromTo(".block", {x:90}, {x: 90});
+
+gsap.fromTo(".balancer", {y:-1000}, {y: 220});
+gsap.fromTo(".balancer", {x:300}, {x: 300});
+
+gsap.to( '.balancer' , { 
+  delay: 1,
+  rotation: 3600,
+  stagger: 1,
+  duration: 6,
+  repeat: -1,
+
+});
+
+gsap.fromTo(".cam1", {y:-1000}, {y: 75});
+gsap.fromTo(".cam1", {x:170}, {x: 170});
+
+gsap.to( '.cam1' , { 
+  delay: 1,
+  rotation: 3600,
+  stagger: 1,
+  duration: 6,
+  repeat: -1,
+
+});
+
+gsap.fromTo(".cam2", {y:-1000}, {y: 75});
+gsap.fromTo(".cam2", {x:180}, {x: 465});
+
+gsap.to( '.cam2' , { 
+  delay: 1,
+  rotation: 3600,
+  stagger: 1,
+  duration: 6,
+  repeat: -1,
+
+});
+
+// gsap.to(".block", { x: 140, y: -100, delay: .1,  });
 
 
 console.log('OK');
