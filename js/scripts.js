1a0d3a68d6788dfaddbdf2e9f7492d5201153f85
CaptHazzard <59583076+CaptHazzard@users.noreply.github.com>
2020-02-24 21:12:55 -0800

    Finalized layout and animations

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,6 @@ gsap.fromTo(".head1", {x:90, delay: 1.5}, {x: 90, delay: 1.5});
 gsap.fromTo(".head2", {y:-3000, delay: 1.5}, {y: 0, delay: 1.5});
 gsap.fromTo(".head2", {x:440, delay: 1.5}, {x: 440, delay: 1.5});
 
-
 gsap.fromTo(".intake", {y:-3000, delay: 1.75}, {y: 5, delay: 1.75});
 gsap.fromTo(".intake", {x:252, delay: 1.75}, {x: 252, delay: 1.75});
 
@@ -18,7 +17,7 @@ gsap.fromTo(".belts", {x:193, delay: 2}, {x: 193, delay: 2});
 gsap.fromTo(".pulleys", {y:-3000, delay: 2.25}, {y: 87, delay: 2.25});
 gsap.fromTo(".pulleys", {x:305, delay: 2.25}, {x: 307, delay: 2.25});
 
-gsap.fromTo(".balancer", {y:-3000, delay: 2.5}, {y: 220, delay: 2.5});
+gsap.fromTo(".balancer", {y:3000, delay: 2.5}, {y: 220, delay: 2.5});
 gsap.fromTo(".balancer", {x:300, delay: 2.5}, {x: 300, delay: 2.5});
 
 gsap.to( '.balancer' , { 
@@ -29,6 +28,9 @@ gsap.to( '.balancer' , {
   repeat: -1,
 
 });
+
+gsap.fromTo(".g", {y:75, delay: 2.75}, {y: 75, delay: 2.75});
+gsap.fromTo(".g", {x:-5000, delay: 2.75}, {x: 5, delay: 2.75});
 
 gsap.fromTo(".cam1", {y:-3000, delay: 2.75}, {y: 75, delay: 2.75});
 gsap.fromTo(".cam1", {x:170, delay: 2.75}, {x: 170, delay: 2.75});
@@ -42,6 +44,12 @@ gsap.to( '.cam1' , {
 
 });
 
+gsap.fromTo(".l", {y:75, delay: 2.25}, {y: 75, delay: 2.25});
+gsap.fromTo(".l", {x:5000, delay: 2.25}, {x: 630, delay: 2.25});
+
+gsap.fromTo(".e", {y:75, delay: 3}, {y: 75, delay: 3});
+gsap.fromTo(".e", {x:5000, delay: 3}, {x: 755, delay: 3});
+
 gsap.fromTo(".cam2", {y:-3000, delay: 3}, {y: 75, delay: 3});
 gsap.fromTo(".cam2", {x:465, delay: 3}, {x: 465, delay: 3});
 
@@ -54,6 +62,11 @@ gsap.to( '.cam2' , {
 
 });
 
+
+gsap.fromTo(".exhaust", {opacity: 0}, {opacity: 1, duration: .75, delay: 3.7, repeat: -1});
+gsap.to (".exhaust", {x:10, delay: 3});
+gsap.to (".exhaust", {y: 280, delay: 3});
+gsap.to (".exhaust", {rotate: 180});
 // gsap.to(".block", { x: 140, y: -100, delay: .1,  });
 
 
